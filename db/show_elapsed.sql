41e51c059218f50573a57b0396aff8a8c1c855b5
Endika Eiros <endika.eiros@gmail.com>
2022-11-28 11:23:15 +0100

    USE employees

--- db/show_elapsed.sql
+++ db/show_elapsed.sql
@@ -1,3 +1,5 @@
+USE employees;
+
 select timediff(
     (select update_time from information_schema.tables where table_schema='employees' and table_name='salaries'),
     (select create_time from information_schema.tables where table_schema='employees' and table_name='employees')
