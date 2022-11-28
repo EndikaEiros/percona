a6fa90dfe21b348d7424319156ab6dfbd77b4b65
Endika Eiros <endika.eiros@gmail.com>
2022-11-29 00:33:21 +0100

    q

--- app/app.py
+++ app/app.py
@@ -4,26 +4,26 @@ import database
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/',methods=['GET'])
 def index():
     lista_db = database.getDBList()
     if len(lista_db)==0:
         lista_db.append('VACIO algo ta mal')
     return render_template('index.html',perconabd=lista_db)
 
-@app.route('/buscar')
-def buscar():
-    return render_template('buscar.html')
+# @app.route('/buscar',methods=['GET'])
+# def buscar():
+#     return render_template('buscar.html')
 
-@app.route('/agregar')
-def agregar():
-    return render_template('agregar.html')
+# @app.route('/agregar',methods=['GET'])
+# def agregar():
+#     return render_template('agregar.html')
 
-@app.route('/borrar')
-def borrar():
-    return render_template('borrar.html')
+# @app.route('/borrar',methods=['GET'])
+# def borrar():
+#     return render_template('borrar.html')
 
-@app.route('/comandolibre')
-def libre():
-    return render_template('libre.html')
+# @app.route('/comandolibre',methods=['GET'])
+# def libre():
+#     return render_template('libre.html')
 
---
+++ app/test.py
@@ -0,0 +1,13 @@
+import mysql.connector as percona
+
+
+_db = percona.connect(
+    host = 'db', user = 'root', password = 'root', port = 3306)
+_c = _db.cursor()
+
+_c.execute('show databases;')
+result = [x for x in _c.fetchall()]
+
+_db.close()
+
+print(result)
