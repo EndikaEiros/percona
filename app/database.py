5f2f108059f1fc225752680c15b18523d8564f27
Endika Eiros <endika.eiros@gmail.com>
2022-11-29 01:10:30 +0100

    a

--- app/database.py
+++ app/database.py
@@ -1,8 +1,7 @@
 import mysql.connector as percona
 
 def getDBList():
-    _db = percona.connect(
-        host = 'db', user = 'root', password = 'root', port = 3306)
+    _db = percona.connect(host = 'db', user = 'root', password = 'root', port = 3306)
     _c = _db.cursor()
 
     _c.execute('show databases;')
