09f42d681691058ef3287896c8bf909d103d2db7
Endika Eiros <endika.eiros@gmail.com>
2022-11-29 18:30:48 +0100

    appy

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import database
+import flask
 
 app = Flask(__name__)
 
@@ -13,17 +14,21 @@ def index():
 
 @app.route('/ejecutarcomando/', methods=['GET','POST'])
 def ejecutarcomando():
-    comando = request.form['comando']
-    db=request.form['dataB']
-
-    print('DB: {}'.format(str(db)))
-    print('Comando: {}'.format(str(comando)))
-    
-    if database.ejecutarComando(db,comando) == 0:
-        lista_db = database.getDBList()
-        if len(lista_db)==0:
-            lista_db.append('No hay ninguna base de datos cargada')
+    if flask.request.method == 'POST':
+        comando = request.form['comando']
+        db=request.form['dataB']
+        print('DB: {}'.format(str(db)))
+        print('Comando: {}'.format(str(comando)))
     else:
-        lista_db=['Ha ocurrido un error']
+        if database.ejecutarComando(db,comando) == 0:
+            lista_db = database.getDBList()
+            if len(lista_db)==0:
+                lista_db.append('No hay ninguna base de datos cargada')
+        else:
+            lista_db=['Ha ocurrido un error']
+        
+        return render_template('index.html',perconabd=lista_db)
     
-    return render_template('index.html',perconabd=lista_db)
+
+    
+
