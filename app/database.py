2763f8f63d6da7a6926f95eb3ee11e3e8899ca98
Endika Eiros <endika.eiros@gmail.com>
2022-11-29 13:59:26 +0100

    a

--- app/database.py
+++ app/database.py
@@ -18,11 +18,16 @@ def getTableList(DB):
     _c = _db.cursor()
 
     _c.execute('show tables;')
+
     result = [x for x in _c.fetchall()]
-    
+
+    for i in range(len(result)):
+        for char in result[i]:
+            if char in "(',)":
+                result[i]= result[i].replace(char,'')
+
     _db.close()
 
-    
     return result
 
 
@@ -42,8 +47,9 @@ def selectAllTable(DB,table):
 def ejecutarComando(DB,comando):
     _db = percona.connect(host = 'db', user = 'root', password = 'root', port = 3306, database=DB)
     _c = _db.cursor()
-
-   
+    
+    print(DB)
+    print(comando)
     _c.execute("'{}';".format(str(comando)))
     
     _db.close()
